ff4ba2141d71f7f1feabb936d9e5b161c9143608
Angelia Campbell <ali.belclamp@gmail.com>
2019-09-10 11:24:34 -0400

    add middleware to reroute to each individual api

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const proxy = require('http-proxy-middleware')
 const port = 4000;
 
 app.use(cors());
 app.use(express.static('public'));
 
+var PhotoCarouselProxy = proxy('/api/', {target: 'http://localhost:3000/'});
+var MoreLikeThisProxy = proxy('/morelikethis/', {target: 'http://localhost:3001/'});
+var ItemOverviewProxy = proxy('/movies/', {target: 'http://localhost:3002/'});
+app.use(MoreLikeThisProxy);
+app.use(PhotoCarouselProxy);
+app.use(ItemOverviewProxy);
 
 app.listen(port, () => {
   console.log('Server is listening at port ', port);
